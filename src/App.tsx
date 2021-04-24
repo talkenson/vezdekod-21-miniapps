dfc61d32a8bc862df4fca827734118ff04da63f4
Vitaly Shatalov <vetka921@gmail.com>
2021-04-25 00:56:14 +0700

    Works

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import vk_logo from './assets/svg/vk_light.svg'
+import Card from './components/Card/Card'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="mx-auto text-center App">
+      <div className="relative mx-auto mt-16 mb-16 flex flex-col justify-center items-center select-none">
+        <div className="vezdekod-text text-white z-30 text-6xl font-medium font-light">
+          вездекод
+        </div>
+        <div className="z-10 underliner" />
+        <img src={vk_logo} className="absolute vk-logo z-30" />
+      </div>
+      <div className="description text-white text-left mx-auto text-xl px-6">
+        <p className="font-medium">
+          В этом году на <code>Вездекоде</code> от ВКонтакте представлены задачи
+          аж из 9-ти категорий!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p className="font-normal mt-4">
+          Среди этих категорий встречаются такие особо интересеные как:
+        </p>
+        <div className="grid grid-cols-1 lg:grid-cols-2 mt-12 mb-12">
+          {[
+            'VK Mini Apps',
+            'Мобильная разработка',
+            'Робототехника',
+            'Информационная безопасность',
+          ].map((v, i) => (
+            <Card title={v} number={i} />
+          ))}
+        </div>
+      </div>
+      <div className="description text-white text-left mx-auto text-xl px-6">
+        <p className="font-medium">
+          В этом году на <code>Вездекоде</code> от ВКонтакте представлены задачи
+          аж из 9-ти категорий!
+        </p>
+        <p className="font-normal mt-4">
+          Среди этих категорий встречаются и такие интересеные:
+        </p>
+        <div className="grid grid-cols-1 lg:grid-cols-2 mt-12 mb-12">
+          {'РУБИКОН, SmartBear, Senla, Веб-интегратор Максимастер, Scloud.ru, DD Planet, Центр информационных технологий, SafeCafe, GDG Russia, Google Dev. Group'
+            .split(',')
+            .map((v) => (
+              <Card title={v} number={2} variant={'mini'} />
+            ))}
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import styles from './Card.module.scss'
+import classNames from 'classnames'
+
+const vals = ['red', 'green', 'blue', 'yellow']
+
+const Card = ({
+  title,
+  number,
+  variant,
+}: {
+  title: string
+  number: number
+  variant?: string
+}) => {
+  const [style, setStyle] = useState('')
+
+  useEffect(() => {
+    setStyle(styles[vals[number]])
+  }, [])
+
+  useEffect(() => {
+    console.log(style)
+  }, [style])
+
+  return (
+    <div
+      className={`relative m-4 mx-auto rounded-2xl card bg-gradient-to-tl 
+      cursor-pointer select-none hover:shadow ${styles.card} ${style} ${
+        styles[variant || 'extended']
+      }`}
+    >
+      <div
+        className={`absolute text-2xl ${
+          variant !== 'mini' ? 'top-20 left-4' : 'top-4 left-4'
+        }`}
+      >
+        {title}
+      </div>
+    </div>
+  )
+}
+
+export default Card
